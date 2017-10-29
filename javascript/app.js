44651496d04b3c87ca7368bce68e5a77e6fa1805
janmilosh <janmilosh@gmail.com>
2017-10-28 21:17:48 -0400

    Improve layout

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,25 @@
+Vue.component('time-entry', {
+  template:
+    '<tr>' +
+      '<td><input type="text" v-bind:value=hourIn>&nbsp;:&nbsp;<input type="text" v-bind:value=minIn></td>' +
+      '<td><input type="text" v-bind:value=hourOut>&nbsp;:&nbsp;<input type="text" v-bind:value=minOut></td>' +
+      '<td>102</td>' +
+    '</tr>',
+  data: function() {
+    return {
+      hourIn: '10',
+      minIn: '01',
+      hourOut: '12',
+      minOut: '21'
+    }
+  }
+})
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    in1: '',
-    out1: '',
-    in2: '',
-    out2: '',
-    in3: '',
-    out3: '',
-    in4: '',
-    out4: '',
+    
     productivity: '',
     txMinutes: ''
   }
