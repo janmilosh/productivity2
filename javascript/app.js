706080131bbef88b5a417e3345ec3c0b3cb88ce5
janmilosh <janmilosh@gmail.com>
2017-11-04 20:52:51 -0400

    First working version

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,7 @@ var app = new Vue({
     productivity: '',
     txMinutes: '',
     totalMinutes: 0,
+    completedMinutes: 0,
     entries: [{
       hourIn: '',
       minIn: '',
@@ -13,6 +14,52 @@ var app = new Vue({
     }]
   },
   methods: {
+    calculateClockOut: function() {
+      var lastIndex = this.entries.length - 1;
+      this.calculateCompletedMinutes();
+      this.totalMinutes = Math.round((Number(this.txMinutes) * 100)/Number(this.productivity));
+      var neededMinutes = this.totalMinutes - this.completedMinutes;
+      var lastClockInMinutes = this.lastClockInMinutes(lastIndex);
+      var lastClockOutMinutes = (neededMinutes + lastClockInMinutes) % 720;
+      if (lastClockInMinutes) {
+        this.entries[lastIndex].minutes = neededMinutes;
+        this.entries[lastIndex].hourOut = Math.floor(lastClockOutMinutes / 60);
+        this.entries[lastIndex].minOut = this.zeroPaddedStr(lastClockOutMinutes % 60);
+      } else {
+        console.log( "Incomplete entry");
+      }
+    },
+    lastClockInMinutes: function(index) {
+      var lastHourIn = this.entries[index].hourIn;
+      var lastMinIn = this.entries[index].minIn;
+      if(this.validHour(lastHourIn) && this.validMinute(lastMinIn)) {
+        return 60 * Number(lastHourIn) + Number(lastMinIn);
+      } else {
+        return false;
+      }
+    },
+    calculateProductivity: function() {
+      for (var i = 0; i < this.entries.length; i++) {
+        if (!this.completeEntry(this.entries[i])) {
+          console.log("Incomplete entry");
+          return false;
+        }
+      }
+      this.calculateTime()
+      this.productivity = ((this.txMinutes * 100)/this.totalMinutes).toFixed(1);       
+    },
+    calculateCompletedMinutes: function() {
+      this.calculateTime();
+      this.completedMinutes = 0;
+      for (var i = 0; i < this.entries.length - 1; i++) {
+        if (!this.completeEntry(this.entries[i])) {
+          console.log("Incomplete entry")
+          return false;
+        } else {
+          this.completedMinutes += this.entries[i].minutes;
+        }
+      }
+    },
     calculateTime: function() {
       var that = this;
       this.totalMinutes = 0;
@@ -21,17 +68,6 @@ var app = new Vue({
         that.totalMinutes += minutes;
         return entry.minutes = minutes;
       });
-      console.log(this.totalMinutes)
-    },
-    calculateProductivity: function() {
-      for (var i = 0; i < this.entries.length; i++) {
-        if (!this.completeEntry(this.entries[i])) {
-          console.log("Incomplete entry")
-          return false;
-        }
-      }
-      this.calculateTime()
-      this.productivity = ((this.txMinutes * 100)/this.totalMinutes).toFixed(1);       
     },
     entryMinutes: function(entry) {
       if (!this.completeEntry(entry)) {
@@ -86,6 +122,14 @@ var app = new Vue({
         this.entries.pop();
       }
       console.log(this.entries);
+    },
+    zeroPaddedStr: function(num) {
+      var numStr = num.toString();
+      if (numStr.length === 1) {
+        return '0' + numStr;
+      } else {
+        return numStr;
+      }
     }
   }
 })
